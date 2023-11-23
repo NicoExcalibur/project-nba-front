591d4ecede9c37967922a0da114d6e26a56214ea
NicoExcalibur <nico.zanz@gmail.com>
2023-11-23 16:31:42 +0100

    WIP get ranks in standings object

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -4,19 +4,47 @@ import { useParams, Link } from "react-router-dom";
 
 function Team() {
   const params = useParams();
+  console.log(params.id);
   const [players, setPlayers] = useState([]);
-  const [currentTeam, setCurrentTeam] = useState([]);
-  console.log(params);
+  // const [currentTeam, setCurrentTeam] = useState([]);
+  const [currentTeamStats, setCurrentTeamStats] = useState([]);
+  const [teamStandings, setTeamStandings] = useState([]);
+  // console.log(params);
 
   const baseURL = process.env.REACT_APP_BASE_URL;
   const apiKey = process.env.REACT_APP_API_KEY;
   const host = process.env.REACT_APP_API_HOST;
 
-  const getCurrentTeam = async () => {
+  // const getCurrentTeam = async () => {
+  //   const options = {
+  //     method: "GET",
+  //     url: baseURL + "/teams",
+  //     params: params,
+  //     headers: {
+  //       "X-RapidAPI-Key": apiKey,
+  //       "X-RapidAPI-Host": host,
+  //     },
+  //   };
+
+  //   await axios
+  //     .request(options)
+  //     .then((response) => {
+  //       console.log(response.data.response[0]);
+  //       setCurrentTeam(response.data.response[0]);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error, "error");
+  //     });
+  // };
+
+  const getCurrentTeamStats = async () => {
     const options = {
       method: "GET",
-      url: baseURL + "/teams",
-      params: params,
+      url: baseURL + "/teams/statistics",
+      params: {
+        id: params.id,
+        season: new Date().getFullYear(),
+      },
       headers: {
         "X-RapidAPI-Key": apiKey,
         "X-RapidAPI-Host": host,
@@ -26,8 +54,34 @@ function Team() {
     await axios
       .request(options)
       .then((response) => {
-        console.log(response.data.response[0]);
-        setCurrentTeam(response.data.response[0]);
+        console.log("teamStats :", response.data.response[0]);
+        setCurrentTeamStats(response.data.response[0]);
+      })
+      .catch((error) => {
+        console.log(error, "error");
+      });
+  };
+
+  const getTeamStandings = async () => {
+    const options = {
+      method: "GET",
+      url: baseURL + "/standings",
+      params: {
+        league: "standard",
+        season: new Date().getFullYear(),
+        team: params.id,
+      },
+      headers: {
+        "X-RapidAPI-Key": apiKey,
+        "X-RapidAPI-Host": host,
+      },
+    };
+
+    await axios
+      .request(options)
+      .then((response) => {
+        console.log("Standings :", response.data.response[0]);
+        setTeamStandings(response.data.response[0]);
       })
       .catch((error) => {
         console.log(error, "error");
@@ -51,7 +105,7 @@ function Team() {
     await axios
       .request(options)
       .then((response) => {
-        console.log(response.data.response);
+        console.log("Players :", response.data.response);
         setPlayers(response.data.response);
       })
       .catch((error) => {
@@ -60,19 +114,34 @@ function Team() {
   };
 
   useEffect(() => {
-    getPlayersList();
+    getTeamStandings();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // useEffect(() => {
+  //   getCurrentTeam();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  useEffect(() => {
+    getCurrentTeamStats();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
-    getCurrentTeam();
+    getPlayersList();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  console.log(currentTeam);
+  console.log(teamStandings);
   return (
     <div className="team-container">
-      <h2>Team {currentTeam.name}</h2>
+      <h2>Team :</h2>
+      <img src="" alt="" />
+      <div className="team-stats">
+        <div className="team-rank">
+          <h3>Rank</h3>
+        </div>
+      </div>
       <div className="players-container">
         {players.map((player) => (
           <div key={player.id} className="player-card">
